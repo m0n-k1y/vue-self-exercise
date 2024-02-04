c1e8c4bf6b769e1e9b381dfd2dc9e5cfeeefea26
hcj <hcj@hcj.com>
2024-02-04 11:37:10 +0800

    xtx 组件注册使用

---
+++ vue-self-2-3/src/components/HotBrandItem.vue
@@ -0,0 +1,20 @@
+<template>
+                <li class="HotBrandItem">
+                  <a href="#">
+                    <img src="@/assets/images/hot1.png" alt="" />
+                  </a>
+                </li>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.HotBrandItem {
+  width: 244px;
+  height: 306px;
+}
+</style>
---
+++ vue-self-2-3/src/components/NewGoodsItem.vue
@@ -0,0 +1,47 @@
+<template>
+              <li class="NewGoodsItem">
+                <a href="#">
+                  <div class="pic"><img src="@/assets/images/goods1.png" alt="" /></div>
+                  <div class="txt">
+                    <h4>KN95级莫兰迪色防护口罩</h4>
+                    <p>¥ <span>79</span></p>
+                  </div>
+                </a>
+              </li>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.NewGoodsItem  {
+  width: 304px;
+  height: 404px;
+  background-color: #EEF9F4;
+}
+.NewGoodsItem  {
+  display: block;
+}
+.NewGoodsItem  .pic {
+  width: 304px;
+  height: 304px;
+}
+.NewGoodsItem  .txt {
+  text-align: center;
+}
+.NewGoodsItem  h4 {
+  margin-top: 17px;
+  margin-bottom: 8px;
+  font-size: 20px;
+}
+.NewGoodsItem  p {
+  font-size: 18px;
+  color: #AA2113;
+}
+.NewGoodsItem  p span {
+  font-size: 22px;
+}
+</style>
